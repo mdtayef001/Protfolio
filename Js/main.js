a67914aecd8fd66822e8c479b632a66a8ae3d3eb
Tayef <tayefhossain200@gmail.com>
2023-08-28 14:22:12 +0600

    added

---
+++ Js/main.js
@@ -0,0 +1,6 @@
+import "../css/modern-normalize.css";
+import "../css/style.css";
+import "../css/sections/header.css";
+import "../css/sections/hero.css";
+import "../css/sections/about.css";
+import "../css/utils.css";
